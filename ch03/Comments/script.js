2e40d2f4ab589059b3ab1ce3b74e9faa1180db90
lfs4 <lfs4@njit.edu>
2013-02-08 12:19:09 -0500

    "new commits"

--- ch03/Comments/script.js
+++ ch03/Comments/script.js
@@ -1,8 +1,19 @@
 var a = 5;
 var b = 10;
+// this is a comment it will not be read by the compiler
+
 var c = 20; 
+// c is a int 
+
+
 var x = true;
+// x and z are bools
 var z = false;
 var name = prompt("What is your name?");
 var message = "Hello, ";
-alert(message + name);
+alert(message+name);
+
+
+
+
+
